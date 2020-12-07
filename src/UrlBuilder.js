145c90fbe0cd062f1c97d9d8d96c56a174bbfc27
Dragos Nedelcu <dragosgn@gmail.com>
2020-12-07 15:49:00 +0100

    small hints

--- src/UrlBuilder.js
+++ src/UrlBuilder.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 import WeatherApiHooks from "./WeatherApiHooks";
+// TODO: move this to an .env file
+// https://create-react-app.dev/docs/adding-custom-environment-variables/
+
 const apiKey = "de93b9fc1363db83f12e17eacffce8f0";
-const apiKey1 = "022585710e14ff3b2155c0af1deebe47";
+
+// const apiKey1 = "022585710e14ff3b2155c0af1deebe47";
 const WEATHER_API = new URL(
   "https://api.openweathermap.org/data/2.5/weather?q=London"
 );
